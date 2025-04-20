9668953e0f78072b31d4b32fa819562b0e7fc325
Erson1989x <erson1989x@gmail.com>
2025-04-20 23:09:38 +0300

    progress

--- index.php
+++ index.php
@@ -31,10 +31,62 @@
         "Game of Thrones"
     ]; ?>
     <ul>
-        <?php  foreach ($books as $book) : ?>
+        <?php foreach ($books as $book) : ?>
             <li><?= $book; ?></li>
         <?php endforeach; ?>
     </ul>
+
+    <li><?= $books[0]; ?></li>
+
+    <?php $books = [
+        [
+            "name" => "The Lord of the Rings",
+            "author" => "L.R. Tolkien",
+            "genre" => "Fantasy",
+            "year" => "1954",
+        ],
+        [
+            "name" => "Harry Potter",
+            "author" => "J.K. Rowling",
+            "genre" => "Fantasy",
+            "year" => "1997",
+        ],
+        [
+            "name" => "Game of Thrones",
+            "author" => "G.R.R. Martin",
+            "genre" => "Fantasy",
+            "year" => "1996",
+        ],
+        [
+            "name" => "The Hitchhiker's Guide to the Galaxy",
+            "author" => "Douglas Adams",
+            "genre" => "Science Fiction",
+            "year" => "1979",
+        ],
+
+    ];
+    function filterByAuthor($books, $author)
+    {
+        $filteredBooks = [];
+        foreach ($books as $book) {
+            if ($book["author"] === $author) {
+                $filteredBooks[] = $book;
+            }
+        }
+        return $filteredBooks;
+    }
+    ?>
+    <ul>
+        <?php foreach (filterByAuthor($books, "G.R.R. Martin") as $book) : ?>
+            <li>
+                <h3><?= $book["name"]; ?></h3>
+                <p>Author: <?= $book["author"]; ?></p>
+                <p>Genre: <?= $book["genre"]; ?></p>
+                <p>Year: <?= $book["year"]; ?></p>
+            </li>
+        <?php endforeach; ?>
+    </ul>
+
 </body>
 
 </html>
